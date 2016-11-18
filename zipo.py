370f62fbe9119564236471eb524aa59a6a64a10d
calvincouch <calvincrouch@Calvins-MBP.hsd1.mn.comcast.net>
2016-11-18 03:01:23 -0600

    add

--- zipo.py
+++ zipo.py
@@ -1,6 +1,6 @@
 import pandas as pd 
-import httplib2
 
-df = pd.read_csv("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_2000.csv")
+
+df = pd.read_csv("/datc/DAT_ASCII_USDJPY_M1_201610.csv")
 
 print df
