07d49a69ee48e55f3ca394498a9ecd9b380466f5
calvincouch <calvincrouch@Calvins-MBP.hsd1.mn.comcast.net>
2016-11-18 02:53:52 -0600

    ad

--- zipo.py
+++ zipo.py
@@ -1,10 +1,8 @@
 import pandas as pd 
+import httplib2
 
+data = httplib2.Http("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_2000.csv")
 
-a = input("year?  ")
-a= str(a)
-gitter = "https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_"+a+".csv"
-
-df = pd.read_csv(gitter)
+df = pd.read_csv(data)
 
 print df
