067e2040af5f1d4fc65c3a7950f71e64c03c2be1
calvincouch <calvincrouch@Calvins-MBP.hsd1.mn.comcast.net>
2016-11-18 02:17:12 -0600

    sd

--- zipo.py
+++ zipo.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 year = input("year?  ")
-gitter = str("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_"+year+".csv")
+gitter = str("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_",input(year),".csv")
 
 df = pd.read_csv(gitter)
 
