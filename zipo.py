9f7381fa97c639218ea485be1e3f41ca4ff268f2
calvincouch <calvincrouch@Calvins-MBP.hsd1.mn.comcast.net>
2016-11-18 02:59:02 -0600

    f

--- zipo.py
+++ zipo.py
@@ -1,8 +1,6 @@
 import pandas as pd 
 import httplib2
 
-data = httplib2.Http("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_2000.csv")
-
-df = pd.read_csv("https://github.com/calvincouch/calvincouch.github.io/blob/master/datc/DAT_ASCII_USDJPY_M1_2000.csv")
+df = pd.read_csv("https://github.com/calvincouch/calvincouch.github.io/datc/DAT_ASCII_USDJPY_M1_2000.csv")
 
 print df
